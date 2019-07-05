57b7f2cec644e7704c14e98045bc88c09e977735
julian <julimastieri@gmail.com>
2019-07-05 16:58:18 -0300

    jjj

--- DistanceVector/src/distanceVector/main.java
+++ DistanceVector/src/distanceVector/main.java
@@ -1,54 +1,83 @@
 package distanceVector;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class main {
 	
 	static final Link LOCAL = new Link(0, 0, null, null);
 	static ArrayList<Link> links = new ArrayList<Link>();
+	static HashMap<Integer,Router> routers = new HashMap<Integer,Router>();
+	static boolean stop = false;
 	
 	public static void main (String[] arg) {
 	
 		//cargar routers de un txt o manualmente por consola
-		Router r1 = new Router(1);
-		Router r2 = new Router(2);
-		Router r3 = new Router(3);
-		Router r4 = new Router(4);
+		crearRouter(1);
+		crearRouter(2);
+		crearRouter(3);
+		crearRouter(4);
 		
-		//asigno las redes de cada uno
-		r1.addRuta("2001:100A::/64", LOCAL, 0);
-		r2.addRuta("2001:100B::/64", LOCAL, 0);
-		r3.addRuta("2001:100C::/64", LOCAL, 0);
-		r4.addRuta("2001:100D::/64", LOCAL, 0);
+		//CREO REDES
+		crearRed(1, "2001:100A::/64");
+		crearRed(2, "2001:100B::/64");
+		crearRed(3, "2001:100C::/64");
+		crearRed(4, "2001:100D::/64");
 		
 		//conecto los routers
-		crearLink(1,1,r1,r2);
-		crearLink(1,1,r1,r2);
-		crearLink(2,1,r1,r3);
-		crearLink(3,1,r2,r4);
-		crearLink(4,1,r3,r4);
+		crearLink(1,1,1,2);
+		crearLink(1,1,1,2);
+		crearLink(2,1,1,3);
+		crearLink(3,1,2,4);
+		crearLink(4,1,3,4);
 		
 		//agrego los adyacentes
+		Integer id;
+		Router r;
+		for ( Entry<Integer, Router> entry : routers.entrySet() ){
+			id = entry.getKey();
+			r = entry.getValue();
+			
+			router.recibirTabla(tabla, costolink);
+		}
 		r1.agregarAdyacente (links);
 		r2.agregarAdyacente (links);
 		r3.agregarAdyacente (links);
 		r4.agregarAdyacente (links);
 		
-		
+		while (!stop){
+			//recorrer lista routers for ()
+		}
+		/*
 		r1.addRuta("2001:100B::/64", l1, 1); //esto se hace cdo se quiere agregar una nueva ruta
 		r2.addRuta("2001:100A::/64", l1, 1);
 		
-		
 		r1.addRuta("2001:100C::/64", l2, 1);
 		r3.addRuta("2001:100A::/64", l2, 1);
+		*/
+		
+		
 		
 		//falta hacer como hacen los routers para descubrrir nevas rutas
 		
 		//guardar si se quiere la topologia de la red creada
 	}
 	
-	public static void crearLink(int id, int costo, Router r1, Router r2){
+	private static void crearRouter(int id) {
+		Router r = new Router(id);
+		routers.put(id, r);
+	}
+
+	public static void crearLink(int id, int costo, int id1, int id2){
+		Router r1 = routers.get(id1);
+		Router r2 = routers.get(id2);
 		Link l = new Link(id, costo, r1, r2);//la declaracion de este link es la representacion de que están conectados
 		links.add(l);
+	}
+	
+	public static void crearRed(int idRouter, String red) {
+		Router r = routers.get(idRouter);
+		r.addRuta(red, LOCAL, 0);
 	}
 }
